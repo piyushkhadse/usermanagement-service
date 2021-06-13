f1ea1344f02c96cc36e48fa89d7d168873819e08
piyushkhadse <piyushkhadse97@gmail.com>
2021-06-13 17:56:57 +0530

    Initial commit

---
+++ src/main/java/com/twitter/usermanagementservice/UsermanagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.twitter.usermanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsermanagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsermanagementServiceApplication.class, args);
+	}
+
+}
